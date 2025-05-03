cf20b60078700ff29214a6d97396aa91a6d58d52
AlexandrPetrenkoTech <aleksandrpetrenkojob@gmail.com>
2025-05-03 19:50:01 +0200

    feat: add entry point (cmd) for the application

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,34 @@ package main
 import (
 	"Advertising/configs"
 	"fmt"
+	"github.com/labstack/echo/v4"
 	"log"
+	"os"
 )
 
 func main() {
+	// Загрузка конфигурации
 	cfg, err := configs.LoadConfig("configs")
 	if err != nil {
 		log.Fatal("error loading config:", err)
 	}
-	fmt.Printf("Server is running on %s:%d\n", cfg.Server.Host, cfg.Server.Port)
+	// Инициализация базы данных и других сервисов
+	dbRepo := repository.NewDBRepository(cfg)
+	advertService := service.NewAdvertService(dbRepo)
+	advertHandler := handler.NewAdvertHandler(advertService)
+
+	//Инициализация Echo
+	e := echo.New()
+
+	//Настройка маршрутов
+	e.POST("/api/adverts", advertHandler.CreateAdvert)
+	e.GET("api/advets/:id", advertHandler.GetAdvertByID)
+	e.GET("/api/adverts", advertHandler.GetAdverts)
+
+	//Запуск сервера
+	log.Printf("Starting server on port %s...", cfg.Server.Port)
+	if err := e.Start(fmt.Sprintf(":%s", cfg.Server.Port)); err != nil {
+		log.Fatal("Error starting server: %v", err)
+		os.Exit(1)
+	}
 }
