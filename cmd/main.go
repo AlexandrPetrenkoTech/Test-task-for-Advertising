fc6193bba02b50ae1a09fd7724c32a3d5ce7afd0
AlexandrPetrenkoTech <aleksandrpetrenkojob@gmail.com>
2025-05-05 16:17:17 +0200

    fix(docker): configure docker-compose with Postgres and fix server start issue

--- cmd/main.go
+++ cmd/main.go
@@ -9,27 +9,27 @@ import (
 )
 
 func main() {
-	// Загрузка конфигурации
+	// Loading configuration
 	cfg, err := configs.LoadConfig("configs")
 	if err != nil {
 		log.Fatal("error loading config:", err)
 	}
-	// Инициализация базы данных и других сервисов
+	// Initializing the database and other services
 	//dbRepo := repository.NewDBRepository(cfg)
 	//advertService := service.NewAdvertService(dbRepo)
 	//advertHandler := handler.NewAdvertHandler(advertService)
 
-	//Инициализация Echo
+	//Initializing Echo
 	e := echo.New()
 
-	//Настройка маршрутов
+	//Setting up routes
 	//e.POST("/api/adverts", advertHandler.CreateAdvert)
 	//e.GET("api/advets/:id", advertHandler.GetAdvertByID)
 	//e.GET("/api/adverts", advertHandler.GetAdverts)
 
-	//Запуск сервера
+	//Starting the server
 	log.Printf("Starting server on port %s...", cfg.Server.Port)
-	if err := e.Start(fmt.Sprintf(":%s", cfg.Server.Port)); err != nil {
+	if err := e.Start(fmt.Sprintf(":%d", cfg.Server.Port)); err != nil {
 		log.Fatal("Error starting server: %v", err)
 		os.Exit(1)
 	}
